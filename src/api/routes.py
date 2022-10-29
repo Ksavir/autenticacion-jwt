da288d3045bb7b4036b9f429e46acf63e2630b75
Ksavir <kevinsavir20@gmail.com>
2022-10-29 00:15:02 +0000

    se realizan rutas y autenticacion con jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,47 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity 
+import datetime
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST', 'GET'])
+def signup():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+
+    body = request.get_json()
+
+    user= User.query.filter_by(email=body['email']).first()
+    if(user):
+        if (user.password == body['password']):
+            
+            expiracion = datetime.timedelta(minutes=1)
+            token = create_access_token(identity=body['email'], expires_delta= expiracion)
+            return jsonify({
+        "email": body['email'],
+        "mensaje" : "Bienvenido",
+        "token": token
+    })
+        else:
+            return jsonify({"mensaje": "Ocurrio un error"})
+    else:
+        return jsonify({"mensaje": "Usuario no existe"})
+
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def autenticacion():
+
+   if request.method == 'GET':
+        iden= get_jwt_identity()
+        return jsonify({"Mensaje ": "Acceso a espacio privado concedido, Bienvenido" , "usuario": iden}), 200
